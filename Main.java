88c8443beb6aba5abe53fc9608aaaf733196f0f1
daniiarr <danraiymkulov@mail.com>
2022-01-12 10:55:34 +0900

    AddTwoNumbers and SearchInsert

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
 package com.company;
 
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        RunningSum rs = new RunningSum();
-        int[] arr = {1, 2, 3, 4};
-        System.out.println(Arrays.toString(rs.runningSum(arr)));
+        ListNode l1 = new ListNode(2);
+        ListNode second = new ListNode(4);
+        ListNode third = new ListNode(3);
+        l1.next = second;
+        l1.next.next = third;
+
+        ListNode l2 = new ListNode(5);
+        ListNode sec = new ListNode(6);
+        ListNode th = new ListNode(4);
+        l2.next = sec;
+        l2.next.next = th;
+
+        AddTwoNumbers atn = new AddTwoNumbers();
+        ListNode res = atn.addTwoNumbers(l1, l2);
+        while (res != null) {
+            System.out.print(res.val);
+            res = res.next;
+        }
     }
 }
