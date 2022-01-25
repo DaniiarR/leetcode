b2099eddf2dfb266e02c3ac74a9d6ef840c04395
daniiarr <danraiymkulov@mail.com>
2022-01-25 23:38:27 +0900

    solved twosum

--- TwoSum.java
+++ TwoSum.java
@@ -1,18 +1,42 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        for (int firstNumberIndex = 0; firstNumberIndex < nums.length;
-             firstNumberIndex++) {
-            for (int i = firstNumberIndex + 1; i < nums.length; i++) {
-                int sum = nums[firstNumberIndex] + nums[i];
-                if (sum == target) {
-                    return new int[]{firstNumberIndex, i};
-                }
+        int start = 0;
+        int end = nums.length - 1;
+        int[] backup = Arrays.copyOf(nums,nums.length);
+        Arrays.sort(nums);
+        int startNum = 0;
+        int endNum = 0;
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum > target) {
+                end--;
+            } else if (sum < target) {
+                start++;
+            } else {
+                startNum = nums[start];
+                endNum = nums[end];
+                break;
             }
         }
-        return null;
+        int[] ans = new int[2];
+        for (int i = 0; i < backup.length; i++) {
+            if (backup[i] == startNum) {
+                ans[0] = i;
+                break;
+            }
+        }
+        for (int i = backup.length - 1; i >= 0; i--) {
+            if (backup[i] == endNum) {
+                ans[1] = i;
+                break;
+            }
+        }
+        return ans;
     }
 
 }
