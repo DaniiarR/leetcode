e81039168852b5c26fd475abf6d9be881a4a144b
daniiarr <danraiymkulov@mail.com>
2022-02-08 10:30:44 +0900

    kthlargest

--- Heap.java
+++ Heap.java
@@ -41,6 +41,15 @@ public class Heap {
         return deletedValue;
     }
 
+    public void sort() {
+        int lastHeapIndex = size - 1;
+        for (int i = 0; i < lastHeapIndex; i++) {
+            int temp = heap[0];
+            heap[0] = heap[lastHeapIndex - i];
+            heap[lastHeapIndex - i] = temp;
+            fixHeapBelow(0, lastHeapIndex - i - 1);
+        }
+    }
     private void fixHeapBelow(int index, int lastHeapIndex) {
         int childToSwap;
         while (index <= lastHeapIndex) {
